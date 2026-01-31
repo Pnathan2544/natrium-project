238bd1dd87b24cd359f6576e7ce16a32527fa2ac
PNathan2544 <Peanats2001@gmail.com>
2026-01-31 16:30:49 +0700

    attributes update and db migration

--- database/migrations/2026_01_30_120802_create_upvotes_table.php
+++ database/migrations/2026_01_30_120802_create_upvotes_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('upvotes', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
