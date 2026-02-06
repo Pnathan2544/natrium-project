02ccbe4952e89bb960655bd9688277329c32f05c
PNathan2544 <Peanats2001@gmail.com>
2026-02-06 15:39:12 +0700

    Routing implementation and PostController init

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,30 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test', function () {
     return view('test-dashboard');
+}); 
+
+*/
+
+Route::middleware(['auth', 'verified'])->group(function() {
+
+    Route::get('/', [PostController::class, 'index'])
+        ->name('dashboard');
+
+    Route::get('/article/create', [PostController::class, 'create'])
+        ->name('article.create');
+
+    Route::post('/article/create', [PostController::class, 'store'])
+        ->name('article.store');
+        
 });
 
 require __DIR__.'/auth.php';
